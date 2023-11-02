4efedb313717ee0f9db973284b11863a5f05602e
Herdanis <herdanis.pradana@gmail.com>
2023-11-02 13:42:56 +0700

    added todo-cli

---
+++ project/todo-cli/model.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+class Todo:
+    def __init__(
+        self,
+        task,
+        catergory,
+        date_added=None,
+        date_completed=None,
+        status=None,
+        position=None,
+    ):
+        self.task = task
+        self.category = catergory
+        self.date_added = (
+            date_added
+            if date_added is not None
+            else datetime.datetime.now().isoformat()
+        )
+        self.date_completed = date_completed if date_completed is not None else None
+        self.status = status if status is not None else 1
+        self.position = position if position is not None else None
+
+    def __repr__(self) -> str:
+        return f"({self.task}, {self.category}, {self.date_added}, {self.date_completed}, {self.status}, {self.position})"
