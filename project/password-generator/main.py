a12c4965bfbf29247512ce49cd4cf7697b0c728a
Herdanis <herdanis.pradana@gmail.com>
2024-05-25 01:21:50 +0700

    update(project): password generator

---
+++ project/password-generator/main.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+letters = list(string.ascii_letters)
+numbers = [str(i) for i in range(10)]
+special_char = ["*", "!", "+", "|", ".", "-", "&"]
+
+print("Password Generator!")
+
+password_list = []
+
+for char in range(0, 9):
+    password_list.append(random.choice(letters))
+
+for char in range(0, 4):
+    password_list.append(random.choice(numbers))
+
+for char in range(0, 2):
+    password_list.append(random.choice(special_char))
+
+random.shuffle(password_list)
+
+password = ""
+
+for char in password_list:
+    password += char
+
+print(f"Here Your the Password: {password}")
